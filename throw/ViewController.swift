1ae121a55d0bfc6e2c1b123903bcf12e89ec4ec1
Logan Arnett <logan@loganarnett.com>
2018-07-19 14:15:45 -0400

    updating with dae models

--- throw/ViewController.swift
+++ throw/ViewController.swift
@@ -13,68 +13,110 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
-    
+	
+	var center : CGPoint!
+	let arrow = SCNScene(named: "art.scnassets/arrow.scn")!.rootNode
+	let gameNode = SCNScene(named: "art.scnassets/scene.scn")!.rootNode
+	var positions = [SCNVector3]()
+	
+	var isGameOn = false
+	
+	
+	func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+		if !isGameOn {
+		let hitTest = sceneView.hitTest(center, types: .featurePoint)
+		let result = hitTest.last
+		guard let transform = result?.worldTransform else {return}
+		let thirdColumn = transform.columns.3
+		let position = SCNVector3Make(thirdColumn.x, thirdColumn.y, thirdColumn.z)
+		positions.append(position)
+		let lastTenPositions = positions.suffix(10)
+		arrow.position = getAveragePosition(from: lastTenPositions)
+		}
+	}
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		if isGameOn {
+			//throw balls
+			createAndThrowFireBall()
+			
+			
+		} else {
+			//place the gameNode
+			guard let angle = sceneView.session.currentFrame?.camera.eulerAngles.y else {return}
+			gameNode.position = arrow.position
+			gameNode.eulerAngles.y = angle
+			isGameOn = true
+			sceneView.scene.rootNode.addChildNode(gameNode)
+			arrow.removeFromParentNode()
+		}
+	}
+	
+	func createAndThrowFireBall() {
+		//create the fireball
+		let ball = SCNSphere(radius: 0.06)
+		let fireBall = SCNNode(geometry: ball)
+		fireBall.physicsBody = .dynamic()
+		fireBall.physicsBody?.mass = 0.5
+		//add particles
+		fireBall.addParticleSystem(SCNParticleSystem(named: "fire.scnp", inDirectory: nil)!)
+		
+		//add the fireball to the scene where the iPhone
+		let camera = sceneView.session.currentFrame?.camera
+		guard let cameraTransform = camera?.transform else {return}
+		fireBall.simdTransform = cameraTransform
+		sceneView.scene.rootNode.addChildNode(fireBall)
+		
+		//set force
+		let force = float4(0, 0,-5,0)
+		let adjustedForce = simd_mul(cameraTransform, force)
+		let vector = SCNVector3Make(adjustedForce.x, adjustedForce.y, adjustedForce.z)
+		fireBall.physicsBody?.applyForce(vector, asImpulse: true)
+		
+		//remove fireball after 3 seconds
+		let disapear = SCNAction.fadeOut(duration: 0.3)
+		fireBall.runAction(.sequence([.wait(duration: 3) ,disapear]))
+		
+	}
+	
+	func getAveragePosition(from positions : ArraySlice<SCNVector3>) -> SCNVector3 {
+		var averageX : Float = 0
+		var averageY : Float = 0
+		var averageZ : Float = 0
+		
+		for position in positions {
+			averageX += position.x
+			averageY += position.y
+			averageZ += position.z
+		}
+		let count = Float(positions.count)
+		return SCNVector3Make(averageX / count , averageY / count, averageZ / count)
+	}
+	
+	
+	
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+		center = view.center
+		sceneView.scene.rootNode.addChildNode(arrow)
+        sceneView.debugOptions = .showPhysicsShapes
     }
+	
+	override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+		center = view.center
+	}
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+	
 }
